f3e41ff11f4c608fd9bd4c5020bdf0ad2dd97b68
Hey Türkiye <138398712+heyturkiye204@users.noreply.github.com>
2024-07-09 11:53:54 +0300

    Update index.tsx

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -12,18 +12,12 @@ export default function Header() {
       >
         <nav className="flex justify-between items-center">
           <div className="w-1/3 flex justify-start">
-            <h1>Roman</h1>
+            <h1><b>Hey Türkiye</b></h1>
           </div>
 
           <div className="w-1/3 flex justify-center gap-x-2 mr-2 sm:mr-0">
             <Link href={"/"}>Home</Link>
             <Link href={"/projects"}>Projects</Link>
-          </div>
-
-          <div className="w-1/3 justify-end hidden sm:flex">
-            <NextLink href={"https://github.com/romanbaba/romanwashere.xyz"} className="bg-[#EDEEF0] text-[#111113] py-1 px-3 rounded-full hover:opacity-80 transition-all duration-200">
-              Get this Template for Free <span>→</span>
-            </NextLink>
           </div>
         </nav>
       </motion.header>
