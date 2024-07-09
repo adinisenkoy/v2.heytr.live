78c1eacaecd82267b983d4101af5e85e15c4fe36
Hey Türkiye <138398712+heyturkiye204@users.noreply.github.com>
2024-07-09 12:04:48 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,14 +5,14 @@ import { GeistSans } from "geist/font/sans";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Roman",
-  description: " Hi, I’m @romanbaba I'm currently learning I am currently living in Adana, Turkey.",
+  title: "Hey Türkiye",
+  description: " Hi, I’m Hey Türkiye.",
   themeColor: "#8B5CF6",
   robots: "index, follow",
   viewport: "width=device-width, initial-scale=1, maximum-scale=1",
-  icons: "https://cdn.discordapp.com/emojis/753954444902924339.png",
+  icons: "https://icons.binaryblazer.me/icons/star/star.svg",
   openGraph: {
-    images: "https://cdn.discordapp.com/emojis/753954444902924339.png",
+    images: "https://icons.binaryblazer.me/icons/star/star.svg",
   },
 };
 
