eab623b30e12eed8e57134390f02c4167b5798be
Hey Türkiye <138398712+heyturkiye204@users.noreply.github.com>
2024-07-09 12:12:57 +0300

    Update index.tsx

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="container mx-auto w-11/12 sm:w-9/12 justify-between flex text-[#858585] selection:text-[#ccc] pb-2 select-text">
-      <span>Copyright © 2021-2024 romanbaba</span>
+      <span>Copyright © 2024 Hey Türkiye</span>
       <span>Made with love by <Link href={"https://github.com/romanbaba"} className="hover:underline hover:text-[#ccc] transition-all duration-200">@romanbaba</Link> </span>
     </footer>
   );
